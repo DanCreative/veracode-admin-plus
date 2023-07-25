3c6d74cb5c82f9f3eef25134b73f690e5fa363ea
Daniel Stadler <danielvstadler@gmail.com>
2023-07-25 21:13:29 +0200

    Update Role model

--- models.go
+++ models.go
@@ -2,7 +2,9 @@ package main
 
 type Role struct {
 	RoleId          string `json:"role_id"`
+	RoleName        string `json:"role_name"`
 	RoleDescription string `json:"role_description"`
+	IsApi           bool   `json:"is_api"`
 	IsChecked       bool   `json:"-"`
 	IsDisabled      bool   `json:"-"`
 }

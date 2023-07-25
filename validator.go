03850f87a53ff0bca4c56546c130c2e8adf4c47a
Daniel Stadler <danielvstadler@gmail.com>
2023-07-25 21:15:28 +0200

    Added ValidateRoles function

---
+++ validator.go
@@ -0,0 +1,24 @@
+package main
+
+func ValidateRoles(user *User) {
+	// Add missing roles
+	newRoles := make([]Role, len(Roles))
+outer:
+	for i, systemRole := range Roles {
+		for _, userRole := range user.Roles {
+			if userRole.RoleId == Roles[i].RoleId {
+				userRole.IsChecked = true
+				newRoles[i] = userRole
+				continue outer
+			}
+		}
+		newRoles[i] = systemRole
+	}
+
+	user.Roles = newRoles
+	// If Admin disable Team Admin
+
+	// If Creator, Security Lead and Submitter, add scan types
+
+	// Sort roles
+}

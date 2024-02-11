0a7c2fb8dd0c5ea955932081f41c03f5509adf42
Daniel Stadler <danielvstadler@gmail.com>
2024-02-11 18:11:23 +0200

    Updated PageMeta to contain params instead of urls

--- user/user.go
+++ user/user.go
@@ -1,5 +1,7 @@
 package user
 
+import "net/url"
+
 type Role struct {
 	RoleId          string
 	RoleName        string
@@ -37,28 +39,49 @@ type PageMeta struct {
 	Size          int
 	TotalElements int
 	TotalPages    int
-	First         string
-	Last          string
-	Next          string
-	Prev          string
-	Self          string
+	FirstParams   string // URL parameters for the first page
+	LastParams    string // URL parameters for the last page
+	NextParams    string // URL parameters for the next page
+	PrevParams    string // URL parameters for the prev page
+	SelfParams    string // URL parameters for the current page
 }
 
 type SearchUserOptions struct {
-	Detailed     string // Passing detailed will return additional hidden fields. Value should be one of: Yes or No
-	Page         int    // Page through the list.
-	Size         int    // Increase the page size.
-	SearchTerm   string // You can search for partial strings of the username, first name, last name, or email address.
-	RoleId       string // Filter users by their role. Value should be a valid Role Id.
-	UserType     string // Filter by user type. Value should be one of: user or api
-	LoginEnabled string // Filter by whether the login is enabled. Value should be one of: Yes or No
-	LoginStatus  string // Filter by the login status. Value should be one of: Active, Locked or Never
-	SamlUser     string // Filter by whether the user is a SAML user or not. Value should be one of: Yes or No
-	TeamId       string // Filter users by team membership. Value should be a valid Team Id.
-	ApiId        string // Filter user by their API Id.
+	Detailed     string `schema:"detailed,omitempty"`      // Passing detailed will return additional hidden fields. Value should be one of: Yes or No
+	Page         int    `schema:"page"`                    // Page through the list.
+	Size         int    `schema:"size,omitempty"`          // Increase the page size.
+	SearchTerm   string `schema:"search_term,omitempty"`   // You can search for partial strings of the username, first name, last name, or email address.
+	RoleId       string `schema:"role_id,omitempty"`       // Filter users by their role. Value should be a valid Role Id.
+	UserType     string `schema:"user_type,omitempty"`     // Filter by user type. Value should be one of: user or api
+	LoginEnabled string `schema:"login_enabled,omitempty"` // Filter by whether the login is enabled. Value should be one of: Yes or No
+	LoginStatus  string `schema:"login_status,omitempty"`  // Filter by the login status. Value should be one of: Active, Locked or Never
+	SamlUser     string `schema:"saml_user,omitempty"`     // Filter by whether the user is a SAML user or not. Value should be one of: Yes or No
+	TeamId       string `schema:"team_id,omitempty"`       // Filter users by team membership. Value should be a valid Team Id.
+	ApiId        string `schema:"api_id,omitempty"`        // Filter user by their API Id.
+	Cart         string `schema:"cart,omitempty"`          // Not part of options for the backend. Value should be one of: Yes or No
 }
 
 type UpdateOptions struct {
 	Incremental *bool // incremental=true indicates that you are adding items to a list for an object property, such as adding users to a team.
 	Partial     *bool // partial=true indicates that you are updating only a subset of properties for an object.
+}
+
+func NewPageMeta(number, size, totalElements, totalPages int, firstUrlStr, lastUrlStr, nextUrlStr, prevUrlStr, selfUrlStr string) PageMeta {
+	firstUrl, _ := url.Parse(firstUrlStr)
+	lastUrl, _ := url.Parse(lastUrlStr)
+	nextUrl, _ := url.Parse(nextUrlStr)
+	prevUrl, _ := url.Parse(prevUrlStr)
+	selfUrl, _ := url.Parse(selfUrlStr)
+
+	return PageMeta{
+		PageNumber:    number,
+		Size:          size,
+		TotalElements: totalElements,
+		TotalPages:    totalPages,
+		FirstParams:   firstUrl.RawQuery,
+		LastParams:    lastUrl.RawQuery,
+		NextParams:    nextUrl.RawQuery,
+		PrevParams:    prevUrl.RawQuery,
+		SelfParams:    selfUrl.RawQuery,
+	}
 }
